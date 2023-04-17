8c36005f97d3a3f7a978bd551b8e3c4b31a8fab1
chipcasla <103225088+chipcasla@users.noreply.github.com>
2023-04-17 17:27:01 -0300

    suma y año bisiesto

---
+++ Lab01.ParaPensar2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01.ParaPensar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anio = int.Parse(Console.ReadLine());
+
+            if (anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0) )
+            {
+                Console.WriteLine("Este año es bisiesto");
+
+            }
+            else
+            {
+
+                Console.WriteLine("Año no bisiesto");
+            }
+
+
+
+            
+        }
+    }
+}
---
+++ Lab01_ParaPensar1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01_ParaPensar1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = int.Parse(System.Console.ReadLine());
+            int numero2 = int.Parse(System.Console.ReadLine());
+            int resultado = numero1 + numero2;
+
+            System.Console.WriteLine("El resultado de la suma de "+numero1+" y "+numero2+" es "+resultado);   
+
+
+        }
+    }
+}
